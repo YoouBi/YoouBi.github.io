2ba96d96ec32aa14b9bccd50bf02d87d66859617
YoouBi <104603807+YoouBi@users.noreply.github.com>
2022-09-08 17:16:57 +0900

    Update main.js

--- 맘스터치/js/main.js
+++ 맘스터치/js/main.js
@@ -5,3 +5,11 @@ $('.main_slider').slick({
     pauseOnHover: false,
     pauseOnFocus: false,
 });
+
+$('.menu_slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+});
