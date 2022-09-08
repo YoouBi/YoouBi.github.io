e3ad103cfa302541ddac4a311e467f5169d1faf7
YoouBi <104603807+YoouBi@users.noreply.github.com>
2022-09-08 17:43:03 +0900

    Update main.js

--- 맘스터치/js/main.js
+++ 맘스터치/js/main.js
@@ -7,9 +7,27 @@ $('.main_slider').slick({
 });
 
 $('.menu_slider').slick({
-    arrows: false,
-    dots: true,
-    autoplay: false,
-    pauseOnHover: false,
-    pauseOnFocus: false,
+    centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
