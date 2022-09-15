2aeb85a09e847c397b8f1d583a58f123c6c4a553
YoouBi <104603807+YoouBi@users.noreply.github.com>
2022-09-15 16:20:42 +0900

    Update main.js

--- 맘스터치/js/main.js
+++ 맘스터치/js/main.js
@@ -10,15 +10,4 @@ $('.menu_slider').slick({
   centerMode: true,
   centerPadding: '160px',
   slidesToShow: 5,
-  responsive: 
-    {
-      breakpoint: 1200,
-      settings: {
-        width: "1200px",
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    }
 });
